56ab6e4c3836a6faea58ce6fe03cecd4e944008e
Bob <olahbalazs@outlook.com>
2019-11-17 19:22:10 +0000

    Create device_info.py

---
+++ device_info.py
@@ -0,0 +1,45 @@
+csr1000v1 = {"device_type": "cisco_ios",
+             "host": "192.168.181.129",
+             "username": "admin",
+             "password": "Cisco123",
+             "port": "22"}
+
+csr1000v2 = {"host": "192.168.181.129",
+             "port": 830,
+             "username": "admin",
+             "password": "Cisco123"}
+
+ios_xe1 = {
+    "host": "ios-xe-mgmt.cisco.com",
+    "port": 10000,
+    "username": "developer",
+    "password": "C1sco12345"
+}
+
+ios_xe_latest = {
+    "host": "ios-xe-mgmt-latest.cisco.com",
+    "port": 10000,
+    "username": "developer",
+    "password": "C1sco12345"
+}
+
+ios_xe2 = {
+    "host": "10.10.20.48",
+    "port": 830,
+    "username": "developer",
+    "password": "C1sco12345"
+}
+
+apicem = {
+    "host": "sandboxapicem.cisco.com",
+    "username": "devnetuser",
+    "password": "Cisco123!",
+    "port": 443
+}
+
+apic = {
+    "host": "https://sandboxapicdc.cisco.com",
+    "username": "admin",
+    "password": "ciscopsdt",
+    "port": 443
+}
