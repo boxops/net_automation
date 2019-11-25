eb669d0c6843c69848b7db0afa45e5aae78ba897
Bob <olahbalazs@outlook.com>
2019-11-25 23:42:00 +0000

    Update and rename iox_guestshell_enable to iox_guestshell_enable.py

--- iox_guestshell_enable.py
+++ iox_guestshell_enable.py
@@ -1,6 +1,8 @@
-# Reference: https://github.com/CiscoDevNet/netprog_basics/blob/master/application_hosting/python_onbox/iosxe_guestshell_setup.txt
+# Reference: https://github.com/CiscoDevNet/netprog_basics/blob/master/application_hosting/python_onbox
+# /iosxe_guestshell_setup.txt
+from netmiko import ConnectHandler
 
-iox = ["conf t",
+iox = ["configure terminal",
        "iox",
        "do sh iox-service",
        "int VirtualPortGroup 0",
@@ -22,5 +24,22 @@ iox = ["conf t",
        "app-default-gateway 192.168.35.1 guest-interface 0",
        "name-server0 208.67.222.222",
        "end",
+       "wr",
        "guestshell enable",
        "show app-hosting list"]
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "192.168.181.129",
+    "username": "admin",
+    "password": "Cisco123",
+    "port": "22"
+}
+
+session = ConnectHandler(**device)
+
+for command in iox:
+    output = session.send_command(command)
+    print(output)
+
+print("Guestshell Configuration Script Done")
