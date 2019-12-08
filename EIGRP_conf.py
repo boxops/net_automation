f625ead5f4cd5227168ffa031de825c776140a02
Bob <olahbalazs@outlook.com>
2019-12-08 00:02:08 +0000

    Update EIGRP_conf.py

--- EIGRP_conf.py
+++ EIGRP_conf.py
@@ -13,8 +13,11 @@ device = {
 
 session = ConnectHandler(**device)
 
+eigrp_configuration_list = []
 interface_names = []
+interface_list = []
 eigrp_networks = []
+eigrp_passive = []
 wildcards = []
 networks = []
 netmasks = []
@@ -23,17 +26,14 @@ netmasks = []
 interfaces = session.send_command("show ip int b")
 for line in interfaces.split("\n"):
     interface_names.append(line.split(" ", 1)[0])
-interface_names.pop(0)  # remove interface header
-# print(interface_names)
+# interface_names.pop(0)  # remove interface header
 
-# get cidr connected networks
+# get cidr connected networks from the routing table
 connected_routes = session.send_command("show ip route | include C")
-connected_networks = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:/\d{1,2}|)", connected_routes)
-# print(connected_networks)
+connected_networks = re.findall(r'[0-9]+(?:\.[0-9]+){3}', connected_routes)
 
-# get netmask from cidr connected networks
+# get network, netmask and wildcard from cidr using netaddr library
 for item in connected_networks:
-    # print(item)
     network = str(netaddr.IPNetwork(item).network)
     networks.append(network)
     netmask = str(netaddr.IPNetwork(item).netmask)
@@ -41,29 +41,48 @@ for item in connected_networks:
     wildcard = str(netaddr.IPNetwork(item).hostmask)
     wildcards.append(wildcard)
 
-# print(networks)
-# print(netmasks)
-# print(wildcards)
-
-# construct eigrp networks
-for item in range(1, len(networks), 1):
+# construct eigrp connected networks
+for item in range(0, len(networks), 1):
     eigrp = str(networks[item] + " " + wildcards[item])
     eigrp_networks.append(eigrp)
 
-print(eigrp_networks)
+eigrp_identifier = input(str("\nEnter a unique identifier number for EIGRP: "))
 
-eigrp_identifier = input(str("Enter a unique identifier number for EIGRP: "))
-eigrp_passive = input((str(interface_names)) + "\nChoose passive interfaces for EIGRP from the list: ")
+loop = True
+while loop:
+    for item in range(1, len(interface_names), 1):
+        print(item, interface_names[item])
+    try:
+        eigrp_passive = int(input("\nEnter passive interface number(s) for EIGRP from the list above, or press any "
+                                  "other key to skip: "))
+        if 1 <= eigrp_passive <= 100:
+            interface_list.append(eigrp_passive)
+    except ValueError:
+        loop = False
 
-eigrp_config_commands = ["router eigrp " + eigrp_identifier,
-                         "no auto-summary",
-                         passive_interfaces,
-                         eigrp_networks]
+print("\n--------------------")
+print("EIGRP Configuration for device: " + session.host)
+print("--------------------")
+eigrp_configuration_list.append("router eigrp " + eigrp_identifier)
+print("router eigrp " + eigrp_identifier)
+eigrp_configuration_list.append("no auto-summary")
+print("no auto-summary")
+for i in interface_list:
+    eigrp_configuration_list.append("passive-interface " + interface_names[i])
+    print("passive-interface " + interface_names[i])
+for j in eigrp_networks:
+    eigrp_configuration_list.append("network " + j)
+    print("network " + j)
+print("--------------------")
 
+apply_configuration = input("\nApply EIGRP configuration? 'yes' to apply, or press any other key to cancel: ")
 
-def device_configuration():
-    print(session.send_config_set(eigrp_config_commands))
+if apply_configuration == 'yes':
+    apply = session.send_config_set(eigrp_configuration_list)
+    print("Applying EIGRP Configuration")
+    print(apply)
+elif apply_configuration != 'yes':
+    print("Rejecting EIGRP Configuration")
 
-
-device_configuration()
 session.disconnect()
+print("\nConfiguration Script Done.")
